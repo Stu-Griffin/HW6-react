718f0c0708c0896d159b26ec9ed12c8424543df2
Max <maksimavramenko33@gmail.com>
2020-09-27 15:01:42 +0300

    Updates

---
+++ precache-manifest.b6dbe5db6c6181695ebc254a953e0d63.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac68a7ad6848b3c111a9d274956878db",
+    "url": "/HW6-react/index.html"
+  },
+  {
+    "revision": "64a7c9b9e4fe2f391ea2",
+    "url": "/HW6-react/static/css/main.06aebbd4.chunk.css"
+  },
+  {
+    "revision": "3bda907a59eba7a61a1b",
+    "url": "/HW6-react/static/js/2.2c890f36.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/HW6-react/static/js/2.2c890f36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64a7c9b9e4fe2f391ea2",
+    "url": "/HW6-react/static/js/main.c1b452ef.chunk.js"
+  },
+  {
+    "revision": "281232496e4a355bd5dc",
+    "url": "/HW6-react/static/js/runtime-main.efc2cd2b.js"
+  },
+  {
+    "revision": "0ec68f32e56d9b9837b8135c646f32e5",
+    "url": "/HW6-react/static/media/comment.0ec68f32.svg"
+  },
+  {
+    "revision": "70ebbcd163470cdcdd46617ad9e91610",
+    "url": "/HW6-react/static/media/likes.70ebbcd1.svg"
+  },
+  {
+    "revision": "10951a97c700476032ce239c7cd8d001",
+    "url": "/HW6-react/static/media/repost.10951a97.svg"
+  },
+  {
+    "revision": "3df4d79c46da0cdf4b8889e9b03564f7",
+    "url": "/HW6-react/static/media/share.3df4d79c.svg"
+  },
+  {
+    "revision": "4ee74cc78373593b14502ab7d6a63faf",
+    "url": "/HW6-react/static/media/tick.4ee74cc7.svg"
+  }
+]);
